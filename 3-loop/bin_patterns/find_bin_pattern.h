ea55d6b54e27a3a6bab2d626137bac9d1ef44ab2
IvanStepanov125 <vanstepanov@yandex.ru>
2023-10-24 23:07:55 +0300

    fff

--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,5 +1,27 @@
-#include <math.h>
+ #include <math.h>
 
 char find_bin_pattern(int number){
-    return 100;
+    int x = number;
+    int flag  = 0;
+    int kol = 0;
+    while (x > 0){
+        if (x % 2 == 1){
+            if (flag == 0)
+                flag += 1;
+            else if (flag == 2){
+                kol += 1;
+                flag = 0;
+                }
+            else
+                flag = 1;
+        }
+        if (x % 2 == 0){
+            if (flag == 1)
+                flag += 1;
+            else
+                flag = 0;
+        }
+        x /= 2;
+        }
+    return kol;
 }
