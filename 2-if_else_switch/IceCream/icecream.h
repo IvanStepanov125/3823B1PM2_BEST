2b54d2fad5daf79b6f93fda0897294d2778ed38d
IvanStepanov125 <vanstepanov@yandex.ru>
2023-10-27 10:15:22 +0300

    fff

--- 2-if_else_switch/IceCream/icecream.h
+++ 2-if_else_switch/IceCream/icecream.h
@@ -3,7 +3,7 @@ int icecream(int x){
         return 0;
     else if ((x % 3 == 0) || (x % 5 == 0))
         return 1;
-    else if (((x % 5 == 3) || (x % 5 == 1)) && (x > 5))
+    else if (((x % 5 == 3) || (x % 5 == 1) || (x % 5 == 4)) && (x > 5))
         return 1;
     else 
         return 0;
