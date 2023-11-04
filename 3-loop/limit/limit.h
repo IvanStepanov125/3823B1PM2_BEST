9e88bfecebd46e790e091997da7e37e3277fa1a1
IvanStepanov125 <vanstepanov@yandex.ru>
2023-11-04 21:24:38 +0300

    ffff

--- 3-loop/limit/limit.h
+++ 3-loop/limit/limit.h
@@ -3,23 +3,21 @@
 double limit(double eps, int n_max){
     double toch = eps;
     int maksim = n_max;
-    if (maksim <= 1){
-        double s = (double)maksim / sqrt((double)maksim * (double)maksim + (double)maksim);
+    double s = 0.0;
+    if (maksim == 1){
+        s = maksim / sqrt(maksim * maksim + maksim);
         return s;
         }
     else{
         double x1 = 0.0;
         double x2 = 0.0;    
         for (int i = 1; i < maksim + 1; i ++){
-            double s = (double)i / sqrt((double)i * (double)i + (double)i);
+            s = i / sqrt(i * i + i);
             x1 = x2;
             x2 = s;
-            printf("%lf %lf %lf\n", x1, x2, toch);
             if (fabs(x2 - x1) <= toch)
                 return s;
         }
     }
-        
-    
-
+    return s;
 }
