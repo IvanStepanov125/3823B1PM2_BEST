bd8e08a5d6d72050cec0e999f98b26fe08ae7442
IvanStepanov125 <vanstepanov@yandex.ru>
2023-11-10 23:11:54 +0300

    fff

--- 4-array/find_hole/hole_finder.h
+++ 4-array/find_hole/hole_finder.h
@@ -1,5 +1,19 @@
 #include <math.h>
 
 int hole_finder(int matrix[], int length, int width){
-    return 0;
+    int otv = 0;
+    int minim = 100000000;
+    int t = 0;
+    for (int i = 1; i < width - 1; i++){
+        for (int j = 1; j < length - 1; j++){
+            t = matrix[i * width + j];
+            if ((matrix[i * width + j - 1] > t) && (matrix[i * width + j + 1] > t) && (matrix[(i - 1) * width + j] > t) 
+            && (matrix[(i - 1) * width + j - 1] > t) && (matrix[(i - 1) * width + j + 1] > t) && (matrix[(i + 1) * width + j] > t) 
+            && (matrix[(i + 1) * width + j - 1] > t) && (matrix[(i + 1) * width + j + 1] > t) && (minim > t)){
+                minim = t;
+                otv = i * 100 + j;
+                }
+        }
+    }
+    return otv;
 }
--- 4-array/kernel/find_kernel.h
+++ 4-array/kernel/find_kernel.h
@@ -1,5 +1,47 @@
 #include <math.h>
+int shislo(int mas1[], int mas2[], int a1, int b1, int a2, int b2, int index){
+    int count = 0;
+    for (int i = 0; i < b2; i ++){
+        for (int j = 0; j < a2; j ++){
+            count += mas1[(i + index / b2) * b2 + j + index % a2] * mas2[i * b2 + j];
+        }
+    }
+    return count;
+}
 
 int find_matrix_kernel(int matrix[], int kernel[], int matrix_size, int kernel_size){
-    return 1;
+    int dl1 = sqrt(matrix_size);
+    int vis1 = sqrt(matrix_size);
+    int dl2 = sqrt(kernel_size);
+    int vis2 = sqrt(kernel_size);
+    int raz1 = dl1 - dl2;
+    int raz2 = vis1 - vis2; 
+    int new_raz = (raz1 + 1) * (raz2 + 1);
+    int flag = 1;
+    while (flag){
+        int dl3 = raz1 + 1;
+        int vis3 = raz2 + 1;
+        int mas[new_raz];
+        for (int i = 0; i < vis3; i++){
+            for (int j = 0; j < dl3; j++){
+                int index = i * dl3 + j; 
+                mas[index] = shislo(matrix, kernel, dl1, vis1, dl2, vis2, index);
+            }
+            
+        }
+        dl1 = dl3;
+        vis1 = vis3;
+        raz1 = dl1 - dl2;
+        raz2 = vis1 - vis2;
+        matrix = mas;
+        if (((raz1 + 1) > dl2) && ((raz2 + 1) > vis2)) 
+            new_raz = (raz1 + 1) * (raz2 + 1);
+        else 
+            flag = 0;
+    }
+    int otv = 0;
+    for (int k = 0; k < new_raz; k++){
+        otv += matrix[k];
+    }
+    return otv;
 }
