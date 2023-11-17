b2f89ff04eff22cd40231324ca53fc08eea7984f
IvanStepanov125 <vanstepanov@yandex.ru>
2023-11-17 14:37:28 +0300

    fff

--- 5-pointer/matrix_multy/matrix.h
+++ 5-pointer/matrix_multy/matrix.h
@@ -7,15 +7,14 @@ int* matrix_multy(int* A, int aM, int aN, int* B, int bM, int bN){
     int mass[minim * minim];
     if ((aN != bN) && (aM != bM))
         return NULL;
-    for (int k = 0; k < minim * minim; k++){
+    for (int j = 0; j < aM; j ++){
         for (int i = 0; i < aN; i ++){
-            for (int j = 0; j < aM; j ++){
-            int bas1 = A[aM * i + j];
-            int bas2 = B[aM * j + i];
-                mass[k] += A[aM * i + j] * B[aM * j + i]; 
-            }
+        int bas1 = A[aM * j + i];
+        int bas2 = B[aM * i + j];
+            mass[??] += A[aM * i + j] * B[aM * j + i]; 
         }
     }
+
     int *p = mass;
     return p;
 }
