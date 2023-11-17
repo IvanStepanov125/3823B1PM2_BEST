a7d9e90564a3c2395e6e8c88f7dd431454a53333
IvanStepanov125 <vanstepanov@yandex.ru>
2023-11-17 21:32:25 +0300

    fff

--- 5-pointer/matrix_multy/matrix.h
+++ 5-pointer/matrix_multy/matrix.h
@@ -1,20 +1,22 @@
 #include <math.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int* matrix_multy(int* A, int aM, int aN, int* B, int bM, int bN){
-    int minim = (int)fmin(aN, aM);
-    int maksim = (int)fmax(aN, aM);
-    int mass[minim * minim];
+    int* mass = (int*)malloc(aM * bM * sizeof(int));
     if ((aN != bN) && (aM != bM))
         return NULL;
-    for (int j = 0; j < aM; j ++){
-        for (int i = 0; i < aN; i ++){
-        int bas1 = A[aM * j + i];
-        int bas2 = B[aM * i + j];
-            mass[??] += A[aM * i + j] * B[aM * j + i]; 
+    for (int j = 0; j < aM * bN; j ++){
+        for (int i = 0; i < aM; i++){
+        int bas1 = A[aM * (j / aM) + i];
+        int bas2 = B[aM * i + (j % bM)];
+        int bas3 = aM * (j / aM) + i;
+        int bas4 = aM * i + (j % bM);
+        int bas5 = A[aM * i + (j / aM)] * B[aM * (j % bM) + i];
+        mass[j] += A[aM * i + (j / aM)] * B[aM * (j % bM) + i]; 
         }
     }
-
-    int *p = mass;
-    return p;
+    
+    return mass;
 }
--- 5-pointer/packaging/pack.h
+++ 5-pointer/packaging/pack.h
@@ -1,9 +1,20 @@
 #include <math.h>
 
 unsigned long long pack_ull(unsigned char arr[], int size){
-    return 100;
+    if (size > 8)
+        return 0;
+    unsigned long long otv = 0;
+    for (int i = 0; i < size; i ++){
+        unsigned long long int j = 1;
+        for (int k = 0; k < i; k ++){
+            j *= 256;
+        }
+        otv += arr[i] * j;
+    }
+    return otv;
 }
 
 unsigned char unpack_ull(unsigned long long input){
-    return 5;
+    unsigned char b1 = (unsigned char)(input >> 32);
+    return b1;
 }
