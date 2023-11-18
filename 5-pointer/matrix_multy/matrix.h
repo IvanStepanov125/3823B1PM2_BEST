a6aff7daba1fe938c5ac74dd3767ef34a4a810f2
IvanStepanov125 <vanstepanov@yandex.ru>
2023-11-18 14:20:40 +0300

    ffff

--- 5-pointer/matrix_multy/matrix.h
+++ 5-pointer/matrix_multy/matrix.h
@@ -5,16 +5,13 @@
 
 int* matrix_multy(int* A, int aM, int aN, int* B, int bM, int bN){
     int* mass = (int*)malloc(aM * bM * sizeof(int));
-    if ((aN != bN) && (aM != bM))
+    if ((aN != bM))
         return NULL;
+    for (int i = 0; i < aM * bN; i++)
+        mass[i] = 0;
     for (int j = 0; j < aM * bN; j ++){
-        for (int i = 0; i < aM; i++){
-        int bas1 = A[aM * (j / aM) + i];
-        int bas2 = B[aM * i + (j % bM)];
-        int bas3 = aM * (j / aM) + i;
-        int bas4 = aM * i + (j % bM);
-        int bas5 = A[aM * i + (j / aM)] * B[aM * (j % bM) + i];
-        mass[j] += A[aM * i + (j / aM)] * B[aM * (j % bM) + i]; 
+        for (int i = 0; i < aN; i++){
+        mass[j] += A[aN * (j / aM) + i] * B[bN * i + (j % bN)]; 
         }
     }
     
--- 5-pointer/scalar_product/mult.h
+++ 5-pointer/scalar_product/mult.h
@@ -1,5 +1,20 @@
 #include <math.h>
 
 int scalar_product(int* vec1, int* vec2, int size){
-    return 100;
+    double pi = 3.1415;
+    int chislitel = 0;
+    for (int i = 0; i < size; i++){
+        chislitel += vec1[i] * vec2[i];
+    }
+    double t1 = 0;
+    double t2 = 0;
+    for (int i = 0; i < size; i++){
+        t1 += vec1[i] * vec1[i];
+        t2 += vec2[i] * vec2[i];
+    }
+    double znamenat = sqrt(t1 * t2);
+    double del = (double)chislitel / (double)znamenat;
+    double otv = acos(del);
+    double otv_kon = otv * 180 / pi;
+    return (int)otv_kon;
 }
