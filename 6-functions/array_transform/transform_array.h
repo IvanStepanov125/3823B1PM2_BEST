d894b7ce78706741b626980128a5ded69ee9c1fa
s23380402 <vanstepanov@uandex.ru>
2023-11-20 14:29:47 +0300

    fff

--- 6-functions/array_transform/transform_array.h
+++ 6-functions/array_transform/transform_array.h
@@ -1,5 +1,29 @@
 #include <math.h>
 
 void transform(double* array, int size, int* comands, int comands_count){
+    for (int i = 0; i < comands_count; i++){
+        if (comands[i] == 1){
+            for (int j = 0; j < size; j ++){
+                array[j] += 1;
+            }
+        }
+        if (comands[i] == 2){
+            for (int j = 0; j < size; j ++){
+                array[j] = sqrt(fabs(array[j]));
+            }
+        }
+        if (comands[i] == 3){
+            
+        }
+        if (comands[i] == 4){
+            
+        }
+        if (comands[i] == 5){
+            
+        }
+        if (comands[i] == 6){
+            
+        }
+    }
     return;
 }
