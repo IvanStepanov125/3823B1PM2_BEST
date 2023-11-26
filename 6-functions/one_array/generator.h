f17c56ef9b1b85bcb8c05ced8dbf4dbff65f414c
IvanStepanov125 <vanstepanov@yandex.ru>
2023-11-27 00:09:19 +0300

    fff

--- 6-functions/one_array/generator.h
+++ 6-functions/one_array/generator.h
@@ -1,5 +1,12 @@
 #include <math.h>
+#include <stdlib.h>
 
 int* array_generator(int* real_size, int k, int m, int f){
-    return 0;
+    *real_size = 12;
+    int* mass = (int*)malloc(*real_size * sizeof(int));
+    for (int i = 0; i < *real_size; i++){
+        mass[i] == 0;
+    }
+    mass[0] = f;
+    return mass;
 }
