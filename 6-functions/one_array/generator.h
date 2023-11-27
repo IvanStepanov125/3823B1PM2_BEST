967fc62af9c3e5b14b94ccb1186afb8f07df48bd
IvanStepanov125 <vanstepanov@yandex.ru>
2023-11-27 15:15:54 +0300

    fff

--- 6-functions/one_array/generator.h
+++ 6-functions/one_array/generator.h
@@ -2,11 +2,25 @@
 #include <stdlib.h>
 
 int* array_generator(int* real_size, int k, int m, int f){
-    *real_size = 12;
+    *real_size = 20;
+    int sum = f;
     int* mass = (int*)malloc(*real_size * sizeof(int));
-    for (int i = 0; i < *real_size; i++){
-        mass[i] == 0;
+    for (int i = 2; i < *real_size; i++){
+        mass[i] = 5;
+        sum += 5;
     }
     mass[0] = f;
+    mass[1] = 0;
+    int j = 0;
+    int a = 0;
+    int b = 0;
+    while (sum > k){
+        a = *real_size - 1 - j;
+        b = mass[*real_size - 1 - j];
+        sum -= mass[*real_size - 1 - j];
+        mass[*real_size - 1 - j] = 0;
+        j ++;
+        }
+
     return mass;
 }
--- 7-recursion/calc_row_sum/sum_series_terms.h
+++ 7-recursion/calc_row_sum/sum_series_terms.h
@@ -1,5 +1,22 @@
 #include <math.h>
+double count = 0.0;
+
+int f(int n){
+    if (n == 1){
+        count += 1 / (double)n; 
+        return 0.0;
+        }
+    else {
+        count += 1 / (double) n;
+        return f(n - 1);
+    }
+}
+
 
 double sum_series(int n){
-    return -1.0;
+    if (n <= 0)
+        return -1;
+    count = 0.0;
+    f(n);
+    return count;
 }
