464cfc9ff55414f2282800717a24c60e7561dab3
IvanStepanov125 <vanstepanov@yandex.ru>
2023-11-29 14:51:11 +0300

    fff

--- 7-recursion/calc_row_sum/sum_series_terms.h
+++ 7-recursion/calc_row_sum/sum_series_terms.h
@@ -2,16 +2,14 @@
 double count = 0.0;
 
 int f(int n){
+    count += 1 / (double)n; 
     if (n == 1){
-        count += 1 / (double)n; 
         return 0.0;
         }
     else {
-        count += 1 / (double) n;
         return f(n - 1);
     }
 }
-
 
 double sum_series(int n){
     if (n <= 0)
--- 7-recursion/new_series/find_series_sum.h
+++ 7-recursion/new_series/find_series_sum.h
@@ -1,5 +1,32 @@
 #include <math.h>
 
+
+double A(int k){
+    if (k == 0)
+        return 0.0;
+    else {
+        return (A(k-1)+1.0)/k;
+    }
+}
+
+
+double F(int k, int t, double count){
+    count += A(t);
+    if (k != t)
+        return F(k, t + 1, count);
+    else 
+        return count;
+}
+
+
 double find_sum_elements_series(int k){
-    return -1.0;
+    double count = 0.0;
+    if (k == 0)
+        return 0;
+    else if (k < 0)
+        return -1;
+    else{
+        count = F(k-1, 0, count);
+        return count;
+    }
 }
