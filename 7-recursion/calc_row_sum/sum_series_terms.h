2738c45c36988359b325e1c5ee9dfef160cb5671
IvanStepanov125 <vanstepanov@yandex.ru>
2023-11-30 23:48:43 +0300

    fff

--- 7-recursion/calc_row_sum/sum_series_terms.h
+++ 7-recursion/calc_row_sum/sum_series_terms.h
@@ -1,20 +1,33 @@
 #include <math.h>
-double count = 0.0;
 
-int f(int n){
-    count += 1 / (double)n; 
-    if (n == 1){
-        return 0.0;
+double f(int n, int k){
+    if (k == 1){
+        return 1.0;
         }
+    else if (n + 1 == k){
+        return 1.0 / k;
+    }
     else {
-        return f(n - 1);
+        return 1.0 / k + f(n, k - 1);
     }
 }
 
 double sum_series(int n){
+    double count = 0.0;
     if (n <= 0)
         return -1;
-    count = 0.0;
-    f(n);
-    return count;
+    if (n > 10000){
+        int flag1 = n /10000;
+        for (int i = 1; i < flag1; i++){
+            count += f((i-1) * 10000, i * 10000);
+        }
+        int flag2 = n % 10000;
+        for (int i = 0; i < flag2; i++){
+            count += f((i-1) + 10000 * flag1 , i + 10000 * flag1);
+        }
+    }
+    else {
+        count += f(0, n);
+    }
+    return count; //21.372839595118059
 }
