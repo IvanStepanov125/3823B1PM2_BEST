e71cdff9474bfd839a242436bd6d9842d33d583f
IvanStepanov125 <vanstepanov@yandex.ru>
2023-12-17 18:42:47 +0300

    fff

--- 9-files/christmas_tree/tree.h
+++ 9-files/christmas_tree/tree.h
@@ -6,45 +6,43 @@
 char* tree_writer(int n){
     FILE* fl;
     fl = fopen("dz.txt", "w");
-    char* per_str = malloc(sizeof(char) * (n + 1));
-    char* vtor_str = malloc(sizeof(char) * (n + 1));
-    char* now = malloc(sizeof(char) * (n + 3));
-    char* new_str = malloc(sizeof(char) * (2 * n - 1));
-    for (int i = 0; i < n; i ++){
-        per_str[i] = ' ';
-        vtor_str[i] = ' ';
-        new_str[i] = ' ';
-        now[i] = ' ';
+    for (int i = 0; i < n-1; i ++){
+        fputc(' ', fl);
     }
-    per_str[n - 1] = '*';
-    per_str[n] = '\0';
-    vtor_str[n - 1] = '^';
-    vtor_str[n] = '\0';
-    new_str[n - 2] = '^';
-    new_str[n - 1] = '|';
-    new_str[n] = '^';
-    new_str[2 * n - 2] = '\0';
-    now[n + 2] = '\0';
-    fprintf(fl, "%s\n", per_str);
-    fprintf(fl, "%s\n", vtor_str);
-    fprintf(fl, "%s\n", new_str);
-    for (int i = 0; i < n - 2; i ++){
-        new_str[n - 3 - i] = '^';
-        new_str[n + 1 + i] = '^';
-        fprintf(fl, "%s\n", new_str);
+    fputc('*', fl);
+    fputc('\n', fl);
+    for (int i = 0; i < n-1; i ++){
+        fputc(' ', fl);
     }
-    now[n - 3] = '_';
-    now[n - 2] = '_';
-    now[n - 1] = ' ';
-    now[n] = '_';
-    now[n + 1] = '_';
-    now[n + 2] = '\0';
-    fprintf(fl, "%s", now);
+    fputc('^', fl);
+    fputc('\n', fl);
+    int k = 0;
+    for (int i = 0; i < n - 1; i ++){
+        k ++;
+        for (int j = 0; j < n - 2 - i; j++){
+            fputc(' ', fl);
+        }
+        for (int j = 0; j < k; j ++){
+            fputc('^', fl);
+        }
+        fputc('|', fl);
+
+        for (int j = 0; j < k; j ++){
+            fputc('^', fl);
+        }
+        fputc('\n', fl);
+    }
+    for (int j = 0; j < n - 3; j++){
+            fputc(' ', fl);
+        }
+    fputc('_', fl);
+    fputc('_', fl);
+    fputc(' ', fl);
+    fputc('_', fl);
+    fputc('_', fl);
+
     fclose(fl);
-    free(per_str);
-    free(vtor_str);
-    free(new_str);
-    free(now);
+    
     char* otv = malloc(sizeof(char) * 100);
     otv[0] = 'd';
     otv[1] = 'z';
--- 9-files/smyle_file/smile_writer.h
+++ 9-files/smyle_file/smile_writer.h
@@ -20,31 +20,47 @@ void write_smile(char filename[], char smile_names[]){
         if ((smile_names[i] == ',') || (i == len - 1)){ 
             i ++;
             str[k] = '\0';
-            if (strcmp(str, "smile") == 0)
-                fprintf(fl, "%s\n", "(n_n)");
-            else if (strcmp(str, "uh...oh") == 0)//"uh...oh"
-                fprintf(fl, "%s\n", "(._.)");
-            else if (strcmp(str, "ouch") == 0) // "ouch"
-                fprintf(fl, "%s\n", "(>_<)");
-            else if (strcmp(str, "happy") == 0)//"happy"
-                fprintf(fl, "%s\n", "(^_^)");
-            else if (strcmp(str, "happier") == 0)//"happier"
-                fprintf(fl, "%s\n", "*(^o^)*");
-            else if (strcmp(str, "sad") == 0)//"sad"
-                fprintf(fl, "%s\n", "(!_!)");
-            else if (strcmp(str, "bored") == 0)//"bored"
-                fprintf(fl, "%s\n", "(=_=)");
-            else if (strcmp(str, "serious") == 0)//"serious"
-                fprintf(fl, "%s\n", "(o_o)");
-            else if (strcmp(str, "wink") == 0)//"wink"
-                fprintf(fl, "%s\n", "(-_o)");
-            else if (strcmp(str, "i messed it up") == 0)//"i messed it up"
-                fprintf(fl, "%s\n", "(-_-:)");
-            else if (strcmp(str, "jealous") == 0)//"jealous"
-                fprintf(fl, "%s\n", "8(>_<)8");
-            else if (strcmp(str, "i love it") == 0)//"i love it"
-                fprintf(fl, "%s\n", "(*-*)");
+            if (strcmp(str, "smile") == 0){
+                fputc('(', fl);
+                fputc('n', fl);
+                fputc('_', fl);
+                fputc('n', fl);
+                fputc(')', fl);
+            }
+            else if (strcmp(str, "uh...oh") == 0){//"uh...oh"
+                fputc('(', fl);
+                fputc('.', fl);
+                fputc('_', fl);
+                fputc('.', fl);
+                fputc(')', fl);
+            }
+            else if (strcmp(str, "ouch") == 0){ // "ouch"
+                fputc('(', fl);
+                fputc('>', fl);
+                fputc('_', fl);
+                fputc('<', fl);
+                fputc(')', fl);
+            }
+            // else if (strcmp(str, "happy") == 0)//"happy"
+            //     fprintf(fl, "%s", "(^_^)");
+            // else if (strcmp(str, "happier") == 0)//"happier"
+            //     fprintf(fl, "%s", "*(^o^)*");
+            // else if (strcmp(str, "sad") == 0)//"sad"
+            //     fprintf(fl, "%s", "(!_!)");
+            // else if (strcmp(str, "bored") == 0)//"bored"
+            //     fprintf(fl, "%s", "(=_=)");
+            // else if (strcmp(str, "serious") == 0)//"serious"
+            //     fprintf(fl, "%s", "(o_o)");
+            // else if (strcmp(str, "wink") == 0)//"wink"
+            //     fprintf(fl, "%s", "(-_o)");
+            // else if (strcmp(str, "i messed it up") == 0)//"i messed it up"
+            //     fprintf(fl, "%s", "(-_-:)");
+            // else if (strcmp(str, "jealous") == 0)//"jealous"
+            //     fprintf(fl, "%s", "8(>_<)8");
+            // else if (strcmp(str, "i love it") == 0)//"i love it"
+            //     fprintf(fl, "%s", "(*-*)");
             k = 0;
+            
         }
     }
     fclose(fl);
