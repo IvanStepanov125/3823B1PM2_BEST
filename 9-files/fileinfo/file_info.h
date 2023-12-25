8d8ecb199ea3b2f7f0ae4f028ccae534a87c9fa2
IvanStepanov125 <vanstepanov@yandex.ru>
2023-12-25 16:13:23 +0300

    fff

--- 9-files/fileinfo/file_info.h
+++ 9-files/fileinfo/file_info.h
@@ -5,32 +5,35 @@
 #include <ctype.h>
 
 unsigned int get_fileinfo(char filename[], unsigned int* abzac_num, unsigned int* num_word){
-    FILE* tr;
-    tr = fopen(filename, "r");
-    printf("%p\n", tr);
-    fflush(stdout);
-    unsigned int col_slov = 0;
-    while (feof(tr) == 0){
-        char* sl = malloc(sizeof(char*) * 150);
-        fscanf(tr, "%s", sl);
-        if (isdigit(sl[0]) == 0)
-            col_slov ++;
-    }
-    rewind(tr);
-    int flag = 0;
-    unsigned int col_abz = 1;
-    unsigned int col_bait = 0;
-    while (feof(tr) == 0){
-        if ((fgetc(tr) == '\n') && (flag == 0)){
-            col_abz ++;
-            flag = 1;
-        }
-        else
-            flag = 0;
-        col_bait ++;
-    }
-    *abzac_num = col_abz;
-    *num_word = col_slov;
-    col_bait --;
-    return col_bait;
+    *abzac_num = 0;
+    *num_word = 0;
+    return 0;
+    // FILE* tr;
+    // tr = fopen(filename, "r");
+    // printf("%p\n", tr);
+    // fflush(stdout);
+    // unsigned int col_slov = 0;
+    // while (feof(tr) == 0){
+    //     char* sl = malloc(sizeof(char*) * 150);
+    //     fscanf(tr, "%s", sl);
+    //     if (isdigit(sl[0]) == 0)
+    //         col_slov ++;
+    // }
+    // rewind(tr);
+    // int flag = 0;
+    // unsigned int col_abz = 1;
+    // unsigned int col_bait = 0;
+    // while (feof(tr) == 0){
+    //     if ((fgetc(tr) == '\n') && (flag == 0)){
+    //         col_abz ++;
+    //         flag = 1;
+    //     }
+    //     else
+    //         flag = 0;
+    //     col_bait ++;
+    // }
+    // *abzac_num = col_abz;
+    // *num_word = col_slov;
+    // col_bait --;
+    // return col_bait;
 }
