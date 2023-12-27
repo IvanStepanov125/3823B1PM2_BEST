c0c148450b9ca6051869cb54f4320b23e27c8afb
IvanStepanov125 <vanstepanov@yandex.ru>
2023-12-28 00:55:48 +0300

    fff

--- 10-structs/paper_please/paper_please_handler.h
+++ 10-structs/paper_please/paper_please_handler.h
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 struct BornDate {
     unsigned char day;
@@ -26,5 +27,16 @@ struct Person
 
 
 unsigned char paper_please_game(struct Person* persons, unsigned char size){
-    return 0;
+    struct Person* now = malloc(sizeof(struct Person*));
+    now = persons;
+    int count = 0;
+    for (int i = 0; i < size; i++){
+        if ((now[i].driver_license == 'B') && ((strcmp(now[i].working_specialty, "programmist") == 0) || (strcmp(now[i].working_specialty, "majort") == 0) || (strcmp(now[i].working_specialty, "collective farmer") == 0))){
+            if ((strcmp(now[i].pasport.country, "Arstocka") ==0) || (strcmp(now[i].pasport.country, "Arcanistan") == 0)){
+                if (2023 - now[i].pasport.bornDate.year < 40)
+                    count ++;
+            }
+        }
+    }
+    return count;
 }
